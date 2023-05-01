23ba0b66c973d1b0f427664c5a23a6ed1b6a5890
Yuni Rizo <yunierrizo@gmail.com>
2023-05-01 15:00:00 -0400

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+    experimental: {
+        appDir: true,
+    },
+    env: {
+        REACT_APP_API_KEY: "rINmkoMvUAKT9Mgzcv7PfqT2OITStCEmYIBHKorjHdc",
+        REACT_APP_API_URI: "https://api.unsplash.com/",
+    },
+};
 
 module.exports = nextConfig
